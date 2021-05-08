f017757f6feefb62bdd20b8e5d950af0b538c26c
Alex Breger <b0558008@mail.sapir.ac.il>
2021-05-08 15:18:41 +0300

    finished complete

--- docs/CharScriptJS.js
+++ docs/CharScriptJS.js
@@ -42,23 +42,18 @@ function topFunction() {
 }
 
 
-// Get the modal
-    var modal = document.getElementById("myModal");
+// Modal
+    var modal = document.getElementById("chadModal");
 
-// Get the image and insert it inside the modal
     var img = document.getElementById("profilePic");
-    var modalImg = document.getElementById("img01");
+    var modalImg = document.getElementById("jumpImg");
     img.onclick = function () {
         modal.style.display = "block";
         modalImg.src = this.src;
     }
 
-// Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     }
-
-
